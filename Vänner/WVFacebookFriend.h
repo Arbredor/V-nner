82e3fdb23985ca03410b6d5bf667fd42be7dbb1c
Arbredor <jonshoe@gmail.com>
2014-05-20 15:12:07 -0700

    With the second fix for bug issue #5, each WVFacebookFriend requests its own image.  It holds a UIImageView just long enough to cache the requested image in AFNetworking’s image cache.  Other objects can request a picture and receive either the cached image or a placeholder.

--- Vänner/WVFacebookFriend.h
+++ Vänner/WVFacebookFriend.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+@class WVFriendsListViewController;
 
 @interface WVFacebookFriend : NSObject
 
@@ -14,12 +15,21 @@
 @property (strong, nonatomic) NSString *fbid;
 @property (strong, nonatomic) NSString *name;
 @property (strong, nonatomic) NSString *pictureURLString;
+@property (strong, nonatomic) NSURLRequest *pictureRequest;
+
+@property (strong, nonatomic) UIImageView *tempImageView;
 
 /* newCopyOfFriend:
  Returns:  A new WVFacebookFriend object with new copies of the strings from
            the provided WVFacebookFriend object.
  */
 + (id)newCopyOfFriend:(WVFacebookFriend *)fbFriend;
+
+/* placeholderImage
+ Returns:  A reference to a UIImage object with the default size friend's
+           image placeholder.  The image is created once and held by the class.
+ */
++ (UIImage *)placeholderImage;
 
 
 /* init:
@@ -34,5 +44,30 @@
            references, or nil if the object couldn't be created properly.
  */
 - (id)initWithIdNameAndPicture:(NSString *)uid :(NSString *)uname :(NSString *)urlString;
+
+
+/* requestImageWithAFNetworking:
+ Uses AFNetworking UIImageView extensions to asynchronously load the friend's
+ picture into a temporary UIImageView (also placing it in the AFNetworking image
+ cache).  On success, asks a non-nil receiver and view to reload the row data if the
+ row is visible, and removes the reference to the temporary UIImageView.
+ */
+- (void)requestImageWithAFNetworking:(NSURLRequest *)request :(WVFriendsListViewController *)receiver :(UITableView *)view :(NSInteger)forRow;
+
+/* cacheImage:
+ Caches the friend's image if it has not already been cached or it has
+ been released from the AFNetworking image cache.  This function creates
+ a temporary UIImageView and calls requestImageWithAFNetworking: using
+ the provided arguments if the image is not in the cache.  Nil arguments
+ for the receiver and view are fine for initial caching.
+ */
+- (void)cacheImage:(NSURLRequest *)request :(WVFriendsListViewController *)receiver :(UITableView *)view :(NSInteger)forRow;
+
+/* picture:
+ Requests this friend's picture from the cache and returns it if it is there.
+ If the picture is not in the cache, it calls cacheImage: with the provided
+ arguments and returns the placeholder image.
+ */
+- (UIImage *)picture:(WVFriendsListViewController *)receiver :(UITableView *)view :(NSInteger)forRow;
 
 @end
--- Vänner/WVFacebookFriend.m
+++ Vänner/WVFacebookFriend.m
@@ -6,7 +6,12 @@
 //  Copyright (c) 2014 SPD. All rights reserved.
 //
 
+#import "WVAppDelegate.h"
+#import "WVAlertsManager.h"
+#import "WVGlobalDefinitions.h"
 #import "WVFacebookFriend.h"
+#import "WVFriendsListViewController.h"
+#import "UIImageView+AFNetworking.h"
 
 @implementation WVFacebookFriend
 
@@ -22,8 +27,86 @@
         _fbid = uid;
         _name = uname;
         _pictureURLString = urlString;
+        _pictureRequest = [NSURLRequest requestWithURL:[NSURL URLWithString:urlString]];
+        _tempImageView = nil;
+        [self cacheImage:_pictureRequest :nil :nil :0];
     }
     return self;
+}
+
+// Cache the friend's image if it has not already been cached or it has been released from the cache
+- (void)cacheImage:(NSURLRequest *)request :(WVFriendsListViewController *)receiver :(UITableView *)view :(NSInteger)forRow {
+    if([[UIImageView sharedImageCache] cachedImageForRequest:request] != nil) {
+        return;  // return if the AFNetworking image cache already has an image for this request
+    }
+    // Create a temporary UIImageView and use the AFNetworking extensions to get the image (and cache it)
+    _tempImageView = [[UIImageView alloc] initWithImage:[WVFacebookFriend placeholderImage]];
+    [self requestImageWithAFNetworking:_pictureRequest :receiver :view :forRow];  // row used to determine if table view should reload
+}
+
+// Return either the friend's cached image or a placeholder.  If the image is not in the cache,
+// request that it be cached.
+- (UIImage *)picture:(WVFriendsListViewController *)receiver :(UITableView *)view :(NSInteger)forRow {
+    NSURLRequest *request = self.pictureRequest;
+    UIImage *image = [[UIImageView sharedImageCache] cachedImageForRequest:request];
+    if(image == nil) {
+        [self cacheImage:request :receiver :view :forRow];  // previous matching AFNetworking requests, if any, will be cancelled
+        image = [WVFacebookFriend placeholderImage];
+    }
+    return image;
+}
+
+
+// Use AFNetworking UIImageView extensions to asynchronously load the friend's picture
+// into a temporary UIImageView (also placing it in the AFNetworking image cache).
+// On success, remove the reference to the temporary UIImageView.
+- (void)requestImageWithAFNetworking:(NSURLRequest *)request :(WVFriendsListViewController *)receiver :(UITableView *)view :(NSInteger)forRow {
+    // doing the weak/strong dance because the view controller and the table view reference the friend objects already
+    // none of these objects should disappear while we are requesting images
+    __weak __typeof(self) weakSelf = self;
+    __weak WVFriendsListViewController *weakFLVC = receiver;
+    __weak UITableView *weakView = view;
+    [_tempImageView setImageWithURLRequest:request
+                          placeholderImage:[WVFacebookFriend placeholderImage]
+                                   success:^(NSURLRequest *request, NSHTTPURLResponse *response, UIImage *image) {
+                                       __strong __typeof(weakSelf) strongSelf = weakSelf;
+                                       __strong WVFriendsListViewController *strongFLVC = weakFLVC;
+                                       __strong UITableView *strongView = weakView;
+                                       if(strongFLVC != nil) {
+                                           // request view to reload the row data if the row is visible
+                                           [strongFLVC pictureCachedForRow:strongView :forRow];
+                                       }
+                                       // image should now be cached - remove the saved reference
+                                      [strongSelf setTempImageView:nil];
+                                   }
+                                   failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error) {
+                                       if(error.code == -999) {  // re-request interrupted async loads - fix for issue #4
+                                           // request again and return immediately (don't recurse rest of function)
+                                           dispatch_async(dispatch_get_main_queue(), ^() {
+                                               __strong __typeof(weakSelf) strongSelf = weakSelf;
+                                               __strong WVFriendsListViewController *strongFLVC = weakFLVC;
+                                               __strong UITableView *strongView = weakView;
+                                               [strongSelf requestImageWithAFNetworking:request :strongFLVC :strongView :forRow];
+                                           });
+                                           return;
+                                       } // otherwise, open an image error alert, if one is not already visible
+                                       WVAlertsManager *alertsManager = [(WVAppDelegate *)([[UIApplication sharedApplication] delegate]) alertsMgr];
+                                       __weak WVAlertsManager *weakAlertsManager = alertsManager;
+                                       [alertsManager imageAlertIfNoneVisible:@"Image load error"
+                                                                             :@"At least one image failed to load.  Press Retry to refresh the data or OK to accept missing images."
+                                                                             :@"Retry"
+                                                                             :@"OK"
+                                                                             :^(NSInteger cancelIndex, NSInteger buttonIndex) {
+                                                                                 __strong WVFriendsListViewController *strongFLVC = weakFLVC;
+                                                                                 __strong WVAlertsManager *strongAlertsManager = weakAlertsManager;
+                                                                                 if(buttonIndex != cancelIndex) {
+                                                                                     if(strongFLVC != nil) {
+                                                                                         [strongFLVC retryAll];
+                                                                                     }
+                                                                                 }
+                                                                                 [strongAlertsManager imageAlertDismissed];
+                                                                             }];
+                                   }];
 }
 
 // Create a new copy with fresh strings
@@ -31,6 +114,16 @@
     return [[WVFacebookFriend alloc] initWithIdNameAndPicture:[NSString stringWithString:[fbFriend fbid]]
                                                              :[NSString stringWithString:[fbFriend name]]
                                                              :[NSString stringWithString:[fbFriend pictureURLString]]];
+}
+
+// Return a UIImage object with the default size friend's image placeholder.
++ (UIImage *)placeholderImage {
+    static UIImage *_placeholderImage = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _placeholderImage = [UIImage imageWithContentsOfFile:WVconst_placeholderImageFile];
+    });
+    return _placeholderImage;
 }
 
 @end
